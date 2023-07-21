14bb0e8a1b878b2ce73d5ad947b6555dd05774fd
Arachne <jhg2819@naver.com>
2023-07-22 00:26:57 +0900

    javascript inline

--- src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -129,6 +129,20 @@ public class BasicController {
         return "basic/comments";
     }
 
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user",new User("UserA",10));
+        addUsers(model);
+
+        return "basic/javascript";
+    }
+
 
     @Data
     public static class User {
